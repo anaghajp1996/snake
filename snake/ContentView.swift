ceb6c77a61b35bee312a16c380f8cd19aad1979e
Anagha <anaghajayaprakash1996@gmail.com>
2023-11-22 19:30:12 -0600

    Initial Commit

---
+++ snake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snake
+//
+//  Created by Anagha K J on 22/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snake/snakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  snakeApp.swift
+//  snake
+//
+//  Created by Anagha K J on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct snakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
