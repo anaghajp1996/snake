e6cdddcb71f334cefc0861f043c73b40044e1628
Anagha <anaghajayaprakash1996@gmail.com>
2023-11-30 23:29:44 -0600

    feat: Detect swipe direction and change snake's movement

--- snake/ContentView.swift
+++ snake/ContentView.swift
@@ -8,33 +8,35 @@
 import SwiftUI
 
 enum Direction {
-    case right
-    case left
-    case up
-    case down
+    case right, left, up, down
 }
 
 @Observable class Snake {
-    var width: CGFloat = 10;
-    var height: CGFloat  = 10;
+    var width: CGFloat = 20;
+    var height: CGFloat  = 20;
     var position = CGPoint(x: 0, y: 0)
     var direction: Direction = .down
     private let maxHeight = UIScreen.main.bounds.height
     private let maxWidth = UIScreen.main.bounds.width
     
-    func moveSnake(inDirection: Direction) {
-        withAnimation(.linear(duration: 3)) {
+    func move(inDirection: Direction) {
+        let duration = ((inDirection == .down) || (inDirection == .up)) ? maxHeight / 60 : maxWidth / 60
+        withAnimation(.linear(duration: duration)) {
             switch inDirection {
                 case .right:
+                    print("right direction")
                     self.position.x = maxWidth
                     break
                 case .left:
+                    print("left direction")
                     self.position.x = 0
                     break
                 case .up:
+                print("up direction")
                     self.position.y = 0
                     break
                 case .down:
+                print("down direction")
                     self.position.y = maxHeight
                     break
             }
@@ -47,16 +49,46 @@ struct ContentView: View {
     private let maxHeight = UIScreen.main.bounds.height
 
     @State var snake = Snake()
+    @State var direction: Direction = .down
     
     var body: some View {
         VStack {
             Rectangle()
-                .fill(.teal)
+                .fill(.indigo)
                 .frame(width: snake.width, height: snake.height).position(snake.position)
-        }.onAppear {
-            snake.moveSnake(inDirection: .right)
         }
-        .padding()
+        .background(Color.white)
+        .onAppear {
+            snake.move(inDirection: direction)
+        }
+        .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
+            .onEnded { value in
+                let start = value.startLocation
+                let end = value.location
+                            
+                let horizontalDistance = end.x - start.x
+                let verticalDistance = end.y - start.y
+                
+                if abs(horizontalDistance) > abs(verticalDistance) {
+                    // Horizontal swipe
+                    if horizontalDistance > 0 {
+                        snake.move(inDirection: .right)
+                        print("right")
+                    } else {
+                        snake.move(inDirection: .left)
+                        print("left")
+                    }
+                } else {
+                    // Vertical swipe
+                    if verticalDistance > 0  {
+                        snake.move(inDirection: .down)
+                        print("down")
+                    } else  {
+                        snake.move(inDirection: .up)
+                        print("up")
+                    }
+                }
+            })
     }
 }
 
