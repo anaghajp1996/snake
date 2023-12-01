07502077d200d5cd486059abd41634db0c171bd4
Anagha <anaghajayaprakash1996@gmail.com>
2023-12-01 00:33:51 -0600

    feat: End and restart snake game

--- snake/ContentView.swift
+++ snake/ContentView.swift
@@ -14,32 +14,52 @@ enum Direction {
 @Observable class Snake {
     var width: CGFloat = 20;
     var height: CGFloat  = 20;
-    var position = CGPoint(x: 0, y: 0)
+    var position = CGPoint(x: 10, y: 10)
     var direction: Direction = .down
+    var showGameOverAlert = false
     private let maxHeight = UIScreen.main.bounds.height
     private let maxWidth = UIScreen.main.bounds.width
+    private var timer: Timer?
+    
+    
+    func startSnake(inDirection: Direction) {
+        let duration = ((inDirection == .down) || (inDirection == .up)) ? maxHeight / 60 : maxWidth / 60
+        print(duration)
+        self.direction = inDirection
+        timer = Timer.scheduledTimer(withTimeInterval: duration / 1000, repeats: true) { _ in
+            self.move(inDirection: self.direction)
+        }
+        RunLoop.current.add(timer!, forMode: .common)
+    }
+    
+    func gameOver() {
+        print("GAME OVERRRRR!!!!!!!!!!!")
+        timer?.invalidate()
+        self.showGameOverAlert = true
+    }
     
     func move(inDirection: Direction) {
-        let duration = ((inDirection == .down) || (inDirection == .up)) ? maxHeight / 60 : maxWidth / 60
-        withAnimation(.linear(duration: duration)) {
+        if self.position.x == maxWidth || self.position.y == maxHeight || self.position.x == 0 || self.position.y == 0 {
+            gameOver()
+        } else {
             switch inDirection {
                 case .right:
-                    print("right direction")
-                    self.position.x = maxWidth
+                    self.direction = .right
+                    self.position.x += 1
                     break
                 case .left:
-                    print("left direction")
-                    self.position.x = 0
+                    self.direction = .left
+                    self.position.x -= 1
                     break
                 case .up:
-                print("up direction")
-                    self.position.y = 0
+                    self.direction = .up
+                    self.position.y -= 1
                     break
                 case .down:
-                print("down direction")
-                    self.position.y = maxHeight
+                    self.direction = .down
+                    self.position.y += 1
                     break
-            }
+                }
         }
     }
 }
@@ -51,15 +71,19 @@ struct ContentView: View {
     @State var snake = Snake()
     @State var direction: Direction = .down
     
+    let backgroundColor = Color.init(red: 128/255, green: 171/255, blue: 100/255)
+    
+    let snakeColor = Color.init(red: 60/255, green: 58/255, blue: 42/255)
+    
     var body: some View {
         VStack {
             Rectangle()
-                .fill(.indigo)
+                .fill(snakeColor)
                 .frame(width: snake.width, height: snake.height).position(snake.position)
         }
-        .background(Color.white)
+        .background(backgroundColor)
         .onAppear {
-            snake.move(inDirection: direction)
+            snake.startSnake(inDirection: .down)
         }
         .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
             .onEnded { value in
@@ -73,22 +97,26 @@ struct ContentView: View {
                     // Horizontal swipe
                     if horizontalDistance > 0 {
                         snake.move(inDirection: .right)
-                        print("right")
                     } else {
                         snake.move(inDirection: .left)
-                        print("left")
                     }
                 } else {
                     // Vertical swipe
                     if verticalDistance > 0  {
                         snake.move(inDirection: .down)
-                        print("down")
                     } else  {
                         snake.move(inDirection: .up)
-                        print("up")
                     }
                 }
             })
+        .alert(Text("Game Over! Oopssss...sss..ss"), isPresented: $snake.showGameOverAlert) {
+            Button("Start Over", role: .cancel) {
+                snake.showGameOverAlert = false
+                snake.position = CGPoint(x: 10, y: 10)
+                snake.startSnake(inDirection: .down)
+            }
+            
+        }
     }
 }
 
